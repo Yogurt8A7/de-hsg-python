34e59d71d3620e499f70573285afd93f1ae0ff00
Yogurt8A7 <haiminhvioedu07@gmail.com>
2025-11-02 13:51:17 +0700

    Improve calculation and formatting of BH and CH
    Refactor calculations and print statements for BH and CH.

--- Đề 5/Đề 5 Bài 3.py
+++ Đề 5/Đề 5 Bài 3.py
@@ -1,7 +1,7 @@
 import math
 AB = int(input("Nhập độ dài cạnh AB: "))
 AH = int(input("Nhập độ dài cạnh AH: "))
-BH = round(math.sqrt((AB ** 2) - (AH ** 2)), 2)
-AC = round(AH**2 / math.sqrt(AB**2 - AH**2), 2)
-print("BH =", BH)
-print("CH =", AC)
+BH = math.sqrt((AB ** 2) - (AH ** 2))
+CH = AH**2 / math.sqrt(AB**2 - AH**2)
+print("BH =", BH:.2f)
+print("CH =", CH:.2f)
